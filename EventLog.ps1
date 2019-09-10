e4c561be272218b918da396b277af200bfb8e4b9
delicious_spaghetti <47301809+dbh201@users.noreply.github.com>
2019-09-10 14:22:30 -0300

    Create EventLog.ps1

---
+++ EventLog.ps1
@@ -0,0 +1,30 @@
+﻿#Arguments are passed using $args array
+
+<#
+------------------
+Arguments are passed using $args array.
+------------------
+There is also a param() function which can better define arguments:
+Param(
+    [Parameter()] <- attributes of parameter (see below)
+    [String[]] <- type of parameter (array of strings in this case)
+    $ComputerName <- name in script scope
+)
+For more information on parameter attributes:
+https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_functions_advanced_parameters?view=powershell-6
+
+------------------
+Here's what a switch looks like in PS:
+Switch (<test-value>)
+{
+    <condition> {<action>}
+    <condition> {<action>}
+}
+------------------
+#>
+switch ($args[1]) {
+    "-list" {
+        foreach ($EventLogName in (get-childitem HKLM:\SYSTEM\CurrentControlSet\Services\EventLog\)) {
+            echo $Name.pschildname;
+            }
+    }
